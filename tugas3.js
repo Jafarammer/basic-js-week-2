87ba28246821bd3778318d4fe164fa9dc6b6abd0
Jafarammer <jafalkaff96@gmail.com>
2022-06-04 22:59:32 +0700

    revisi tugas  no 3 minggu ke 2

--- tugas3.js
+++ tugas3.js
@@ -1,19 +1,20 @@
 const data = [2, 25, 4, 14, 17, 30, 8];
-function orderData(nilaiAwal, nilaiAkhir, dataArray) {
+const orderData = function (nilaiAwal, nilaiAkhir, dataArray) {
   if (nilaiAwal < nilaiAkhir) {
-    if (dataArray.length > 5) {
-      const filtered = dataArray.filter(
-        (y) => y >= nilaiAwal && y <= nilaiAkhir
-      );
-      console.log(filtered);
-    } else if (dataArray.length < 5) {
-      console.log("Data array harus lebih dari 5");
+    const filtered = dataArray.filter(function (hasil) {
+      return hasil > nilaiAwal && hasil < nilaiAkhir;
+    });
+    if (dataArray.length < 5) {
+      console.log("Jumlah angka dalam array harus lebih dari 5");
+    } else if (filtered == 0) {
+      console.log("Nilai tidak ditemukan");
     } else {
-      console.log("Nilai kosong");
+      console.log(filtered);
     }
-  } else {
+  } else if (nilaiAwal > nilaiAkhir) {
     console.log("Nilai akhir harus lebih besar dari nilai awal");
+  } else {
+    console.log("Data harus angka!");
   }
-}
-
+};
 orderData(5, 20, data);
