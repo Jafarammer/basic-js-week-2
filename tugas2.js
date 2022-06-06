5256712132ab041996f13c61d26ec38a7a33e26e
Jafarammer <jafalkaff96@gmail.com>
2022-06-06 07:58:23 +0700

    revisi tugas 2 minggu ke 2

--- tugas2.js
+++ tugas2.js
@@ -18,13 +18,18 @@ const cariNama = [
 ];
 
 const filterItems = (arr, query) => {
-  return arr.filter(
-    (param) => param.toLowerCase().indexOf(query.toLowerCase()) !== -1
-  );
+  const filtered = arr.filter(function (hasil) {
+    return hasil.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  });
+  if (filtered == 0) {
+    return "Nama tidak ditemukan";
+  } else {
+    const temp = [];
+    const al = filtered;
+    for (let i = 0; i < 3; i++) {
+      temp.push(al[i]);
+    }
+    return temp;
+  }
 };
-const al = filterItems(cariNama, "o");
-const temp = [];
-for (let i = 0; i < 3; i++) {
-  temp.push(al[i]);
-}
-console.log(temp);
+console.log(filterItems(cariNama, "jafar"));
